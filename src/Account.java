d97ac5b1aad050ca11f73979f1d341318c177837
Derek-Vergara <derek.anveor@gmail.com>
2026-03-24 15:39:26 -0600

    Initial structure and creation of the Account class

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+//Account class that contains instance variable 
+// and methods to set and get its value 
+
+public class Account {
+
+    private String name; //instance variable
+
+    //method to set the name in the object 
+    public void setName (String name){
+
+    this.name = name; //store the name 
+    }// end of setName
+
+    public String getName ()
+    {
+        return name; //return value of name to caller
+    }
+
+}//End of class Account
